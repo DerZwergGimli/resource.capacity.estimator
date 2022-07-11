be0b56cac49379d2b7acfe8dd4f6f639bcb0130a
DZWG <paule22@protonmail.com>
2022-07-11 12:04:52 +0200

    fixed possibly undefined

--- src/components/special/OverviewCard.vue
+++ src/components/special/OverviewCard.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="border-2 border-cyan-800">
+  <div class="border-2 rounded-tr-xl rounded-bl-xl border-cyan-800 w-full">
     <div>
       {{
         storage.hostsList.find((host) =>
-          host.uuids.includes(assignment.host_uuid)
-        ).name
+          host.uuids.includes(assignment?.host_uuid ?? "none")
+        )?.name ?? "error"
       }}
     </div>
     <table class="table table-compact w-full">
@@ -12,37 +12,42 @@
         <tr>
           <th></th>
           <th>VM-Name</th>
-          <th>CPU</th>
-          <th>RAM</th>
-          <th>Storage</th>
+          <th class="text-right">CPU</th>
+          <th class="text-right">RAM</th>
+          <th class="text-right">Storage</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(vm_uuid, index) in assignment.vm_uuid" :key="vm_uuid">
+        <tr v-for="(vm_uuid, index) in assignment?.vm_uuid" :key="vm_uuid">
           <th>{{ index }}</th>
           <td>
-            {{ storage.vmsList.find((vm) => vm.uuids.includes(vm_uuid)).name }}
-          </td>
-          <td class="text-right">
             {{
-              storage.vmsList.find((vm) => vm.uuids.includes(vm_uuid)).vcpu[
-                storage.system_recommendation
-              ]
+              storage.vmsList.find((vm) => vm.uuids.includes(vm_uuid))?.name ??
+              "error"
             }}
           </td>
           <td class="text-right">
             {{
-              storage.vmsList.find((vm) => vm.uuids.includes(vm_uuid)).vram[
-                storage.system_recommendation
+              storage.vmsList.find((vm) => vm.uuids.includes(vm_uuid))?.vcpu[
+                storage.system_recommendation ?? "error"
               ]
             }}
+            Cores
           </td>
           <td class="text-right">
             {{
-              storage.vmsList.find((vm) => vm.uuids.includes(vm_uuid)).vstorage[
-                storage.system_recommendation
+              storage.vmsList.find((vm) => vm.uuids.includes(vm_uuid))?.vram[
+                storage.system_recommendation ?? "error"
               ]
             }}
+            GB
+          </td>
+          <td class="text-right">
+            {{
+              storage.vmsList.find((vm) => vm.uuids.includes(vm_uuid))
+                ?.vstorage[storage.system_recommendation] ?? "error"
+            }}
+            GB
           </td>
         </tr>
       </tbody>
--- src/views/OverviewView.vue
+++ src/views/OverviewView.vue
@@ -4,7 +4,7 @@
       <h1>Overview Configuration</h1>
       <div class="flex flex-row space-x-2"></div>
     </div>
-    <div class="grid m-4 py-2 card bg-base-300 rounded-box place-items-center">
+    <div class="grid grid-cols-2 gap-4 m-4">
       <div v-for="assignment in storage.assignmentsList" :key="assignment">
         <OverviewCard :assignment="assignment"></OverviewCard>
       </div>
