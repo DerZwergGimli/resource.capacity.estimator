4917bd44b23adaf4e0f7d0eb75444ecb51d39e4f
DZWG <dzwg@protonmail.com>
2022-07-26 17:19:16 +0200

    new printout View

--- src/views/PrintOutView.vue
+++ src/views/PrintOutView.vue
@@ -19,11 +19,15 @@ const storage = appStorage();
 storage.init();
 
 let vms_list = storage.vmsList.map((vms, index) => {
-  return [index, vms.name, vms.os, vms.amount];
+  return [index, vms.name, vms.os, vms.uuids.length];
 });
 
 let hosts_list = storage.hostsList.map((host, index) => {
-  return [index, host.name, host.manufacturer, host.amount];
+  return [index, host.name, host.manufacturer, host.uuids.length];
+});
+
+let assignments_list_pre = storage.assignmentsList.map((assignment, index) => {
+  return [index, assignment.host_uuid, assignment.vm_uuid];
 });
 
 /*storage.assignmentsList.sort(function (a, b) {
@@ -31,7 +35,7 @@ let hosts_list = storage.hostsList.map((host, index) => {
 });*/
 
 function generatePDF() {
-  const doc = new jsPDF();
+  const doc = new jsPDF({ orientation: "landscape" });
 
   doc.text("VM-Table", 15, 10);
   autoTable(doc, {
@@ -44,6 +48,14 @@ function generatePDF() {
   autoTable(doc, {
     head: [["ID", "Host-Name", "Manufacturer", "Amount"]],
     body: hosts_list,
+  });
+
+  assignments_list_pre.forEach((assignment_pre) => {
+    doc.addPage();
+    doc.text("Assignment-Table" + assignments_list_pre[1], 15, 10);
+    autoTable(doc, {
+      head: [["ID", "VM-Name", "Manufacturer", "Amount"]],
+    });
   });
 
   doc.save("Resource_Calculator_Export.pdf");
